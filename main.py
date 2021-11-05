4cbe1a8d02dc54d045bc2f9bb0cd08c8f89d17a8
LEESANGNAM <fgh0116@naver.com>
2021-11-05 15:43:40 +0900

    DB file Insert

--- main.py
+++ main.py
@@ -1,40 +1,40 @@
 import psycopg2
 
+def File_insert(filename,tablename):
+    try:
+        # 파일명 받아와서 열기
+        f = open(filename,'r',encoding='cp949')
+        cur.copy_from(f, tablename, sep='|') # 받아온 데이터를 입력받은 테이블에 넣기 sep: 구분자.
+    except FileNotFoundError as fileError:
+        print(fileError,"파일이없습니다.")
+    except Exception as e:
+        print(e)
+    finally:
+        f.close()
+        connection.commit()
+
+# read  ( select 결과를 리턴)
+def readDB(schema, table):
+    sql = " SELECT * from {schema}.{table}".format(schema=schema, table=table)
+    try:
+        cur.execute(sql)
+        result = cur.fetchall()
+    except Exception as e:
+        result = (" read DB err", e)
+    return result
 # db 연결
-connection = psycopg2.connect(host="172.30.6.41", dbname="geoproject", user="postgres", password="all4land", port=5432)
+try:
+    connection = psycopg2.connect(host="172.30.6.41", dbname="geoproject", user="postgres"
+                              , password="all4land", port=5432, options="-c search_path=lsn")
+except Exception as conError:
+        print("연결실패")
+else:
+    cur = connection.cursor()
+    File_insert('/Users/isangnam/PycharmProjects/geoproject/road_code_Sejong.txt', 'road_code')
 
-cur = connection.cursor()
-# 커서에 객체가 있다면 연결성공
-if cur != None:
-    print("연결성공")
-
-f = open(r'C:UsersnDesktopdata.csv', 'r')
-cur.copy_from(f, temp_unicommerce_status, sep=',')
-
-
-#
-# # insert
-# def insertDB(schema, table,data):
-#     sql = " INSERT INTO {schema}.{table} VALUES ({data}) ;".format(schema=schema, table=table,
-#                                                                                data=data)
-#     try:
-#         cur.execute(sql)
-#         connection.commit()
-#     except Exception as e:
-#         print(" insert DB err ", e)
-# # read  ( select 결과를 리턴)
-# def readDB(schema, table):
-#     sql = " SELECT * from {schema}.{table}".format(schema=schema, table=table)
-#     try:
-#         cur.execute(sql)
-#         result = cur.fetchall()
-#     except Exception as e:
-#         result = (" read DB err", e)
-#
-#     return result
-# insertDB("lsn","road_code","'4','2','3','7','5','6','7','8','9','0','1','2','3','4','5','6','7'")
-# print(readDB("lsn","road_code"),end=" ")
-#
-# cur.close()
-# connection.close()
-#
+    # rows = readDB("lsn", "road_code")
+    # for i in rows:
+    #     print(i)
+finally:
+    cur.close()
+    connection.close()
